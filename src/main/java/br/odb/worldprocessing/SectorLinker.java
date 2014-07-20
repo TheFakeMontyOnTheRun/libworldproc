6462518d35fd53c9436601769a7c9a3624ea0695
Daniel Monteiro <danielmonteiro@id.uff.br>
2014-07-20 18:50:28 -0300

    updating for Directions instead of a loose constant pool

--- src/main/java/br/odb/worldprocessing/SectorLinker.java
+++ src/main/java/br/odb/worldprocessing/SectorLinker.java
@@ -4,6 +4,7 @@
 package br.odb.worldprocessing;
 
 import br.odb.gameapp.ApplicationClient;
+import br.odb.gameworld.Direction;
 import br.odb.libscene.Constants;
 import br.odb.libscene.Sector;
 import br.odb.libscene.World;
@@ -53,8 +54,8 @@ public class SectorLinker implements WorldProcessor {
 				&& Utils.eqFloat(s1.getZ1(), s2.getZ1())
 				&& Utils.eqFloat(s1.getY0(), s2.getY0())
 				&& Utils.eqFloat(s1.getY1(), s2.getY1())) {
-			s1.setLink(Constants.FACE_W, s2.getId());
-			s2.setLink(Constants.FACE_E, s1.getId());
+			s1.setLink(Direction.W, s2.getId());
+			s2.setLink(Direction.E, s1.getId());
 		}
 
 		if (Utils.eqFloat(s1.getX1(), s2.getX0())
@@ -62,8 +63,8 @@ public class SectorLinker implements WorldProcessor {
 				&& Utils.eqFloat(s1.getZ1(), s2.getZ1())
 				&& Utils.eqFloat(s1.getY0(), s2.getY0())
 				&& Utils.eqFloat(s1.getY1(), s2.getY1())) {
-			s1.setLink(Constants.FACE_E, s2.getId());
-			s2.setLink(Constants.FACE_W, s1.getId());
+			s1.setLink(Direction.E, s2.getId());
+			s2.setLink(Direction.W, s1.getId());
 		}
 
 		if (Utils.eqFloat(s1.getY0(), s2.getY1())
@@ -71,8 +72,8 @@ public class SectorLinker implements WorldProcessor {
 				&& Utils.eqFloat(s1.getZ1(), s2.getZ1())
 				&& Utils.eqFloat(s1.getX0(), s2.getX0())
 				&& Utils.eqFloat(s1.getX1(), s2.getX1())) {
-			s1.setLink(Constants.FACE_FLOOR, s2.getId());
-			s2.setLink(Constants.FACE_CEILING, s1.getId());
+			s1.setLink(Direction.FLOOR, s2.getId());
+			s2.setLink(Direction.CEILING, s1.getId());
 		}
 
 		if (Utils.eqFloat(s1.getY1(), s2.getY0())
@@ -80,8 +81,8 @@ public class SectorLinker implements WorldProcessor {
 				&& Utils.eqFloat(s1.getZ1(), s2.getZ1())
 				&& Utils.eqFloat(s1.getX0(), s2.getX0())
 				&& Utils.eqFloat(s1.getX1(), s2.getX1())) {
-			s1.setLink(Constants.FACE_CEILING, s2.getId());
-			s2.setLink(Constants.FACE_FLOOR, s1.getId());
+			s1.setLink(Direction.CEILING, s2.getId());
+			s2.setLink(Direction.FLOOR, s1.getId());
 		}
 
 		if (Utils.eqFloat(s1.getZ0(), s2.getZ1())
@@ -89,8 +90,8 @@ public class SectorLinker implements WorldProcessor {
 				&& Utils.eqFloat(s1.getX1(), s2.getX1())
 				&& Utils.eqFloat(s1.getY0(), s2.getY0())
 				&& Utils.eqFloat(s1.getY1(), s2.getY1())) {
-			s1.setLink(Constants.FACE_N, s2.getId());
-			s2.setLink(Constants.FACE_S, s1.getId());
+			s1.setLink(Direction.N, s2.getId());
+			s2.setLink(Direction.S, s1.getId());
 		}
 
 		if (Utils.eqFloat(s1.getZ1(), s2.getZ0())
@@ -98,8 +99,8 @@ public class SectorLinker implements WorldProcessor {
 				&& Utils.eqFloat(s1.getX1(), s2.getX1())
 				&& Utils.eqFloat(s1.getY0(), s2.getY0())
 				&& Utils.eqFloat(s1.getY1(), s2.getY1())) {
-			s1.setLink(Constants.FACE_S, s2.getId());
-			s2.setLink(Constants.FACE_N, s1.getId());
+			s1.setLink(Direction.S, s2.getId());
+			s2.setLink(Direction.N, s1.getId());
 		}
 	}
 
--- src/main/java/br/odb/worldprocessing/SectorSnapper.java
+++ src/main/java/br/odb/worldprocessing/SectorSnapper.java
@@ -4,6 +4,7 @@
 package br.odb.worldprocessing;
 
 import br.odb.gameapp.ApplicationClient;
+import br.odb.gameworld.Direction;
 import br.odb.gameworld.exceptions.InvalidSlotException;
 import br.odb.libscene.Constants;
 import br.odb.libscene.Sector;
@@ -35,57 +36,45 @@ public class SectorSnapper implements WorldProcessor {
 
 		Sector sector;
 		int link;
+		for (Direction d : Direction.values()) {
 
-		for (int d = 0; d < 6; ++d) {
+			link = current.getLink(d);
 
-			try {
-				link = current.getLink(d);
+			if (link != Constants.NO_LINK) {
 
-				if (link != Constants.NO_LINK) {
+				if (link < 0 || link > world.getTotalSectors()) {
 
-					if (link < 0 || link > world.getTotalSectors()) {
+					client.printError("Sector " + current
+							+ " has a wrong link.");
 
-						client.printError("Sector " + current
-								+ " has a wrong link.");
+				}
 
-					}
+				sector = world.getSector(link);
 
-					sector = world.getSector(link);
+				synchronized (sector) {
 
-					synchronized (sector) {
+					switch (d) {
 
-						switch (d) {
-
-						case Constants.FACE_N:
-							current.setZ0(sector.getZ1());
-							break;
-						case Constants.FACE_S:
-							current.setZ1(sector.getZ0());
-							break;
-						case Constants.FACE_W:
-							current.setX0(sector.getX1());
-							break;
-						case Constants.FACE_E:
-							current.setX1(sector.getX0());
-							break;
-						case Constants.FACE_FLOOR:
-							current.setY0(sector.getY1());
-							break;
-						case Constants.FACE_CEILING:
-							current.setY1(sector.getY0());
-							break;
-						}
+					case N:
+						current.setZ0(sector.getZ1());
+						break;
+					case S:
+						current.setZ1(sector.getZ0());
+						break;
+					case W:
+						current.setX0(sector.getX1());
+						break;
+					case E:
+						current.setX1(sector.getX0());
+						break;
+					case FLOOR:
+						current.setY0(sector.getY1());
+						break;
+					case CEILING:
+						current.setY1(sector.getY0());
+						break;
 					}
 				}
-			} catch (InvalidSlotException e) {
-
-				if (client != null
-						&& LogUtils.getLevel() == LogUtils.LEVEL_VERBOSE) {
-
-					client.printError("Error - Invalid Slot. Sector: "
-							+ current + " slot: " + d + " exception: " + e);
-				}
-				e.printStackTrace();
 			}
 		}
 
